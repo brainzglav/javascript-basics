f874aa2b67957c52a16d50f5c708b2c0f7a9b2a4
marzglav <feg.leg.crypto@gmail.com>
2022-04-04 20:46:46 +0200

    Date formatting

--- date.js
+++ date.js
@@ -1,5 +1,5 @@
 const today = new Date();
-
+/* 
 // ISO date
 console.log(today);
 // UTC date
@@ -9,4 +9,35 @@ console.log(today.getTime());
 console.log(today.getTimezoneOffset());
 
 const withoutTimezone = today - today.getTimezoneOffset() * 60000;
-console.log(new Date(withoutTimezone));
+console.log(new Date(withoutTimezone)); 
+*/
+
+//Ispisi mi vrime u ovakvom obliku April, 04 18:15:50 2022
+
+const EMonthName = {
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+};
+
+today.getFullYear();
+
+function formatNumber(num) {
+  return num < 10 ? `0${num}` : num;
+}
+
+function formatDate(date) {
+  const month = EMonthName[date.getMonth()];
+  const day = formatNumber(date.getUTCDate());
+  const hour = formatNumber(date.getHours());
+  const minutes = formatNumber(date.getMinutes());
+  const seconds = formatNumber(date.getSeconds());
+  const year = date.getFullYear();
+
+  return `${month}, ${day} ${hour}:${minutes}:${seconds} ${year}`;
+}
+
+const formattedDate = formatDate(today);
+
+console.log(formattedDate);
